07acf5f6461164e166df050b39a1eac86191b9a6
devgeetech <devgeetech@gmail.com>
2019-04-15 16:33:39 +0530

    firebase + build

---
+++ build/precache-manifest.de8c9f574e2e208dfe4c411d70183c36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "72ad3281763383c91e36",
+    "url": "/static/js/main.bfe69dfa.chunk.js"
+  },
+  {
+    "revision": "f471c69b87bd1039be55",
+    "url": "/static/js/2.f72d60eb.chunk.js"
+  },
+  {
+    "revision": "72ad3281763383c91e36",
+    "url": "/static/css/main.8ec39754.chunk.css"
+  },
+  {
+    "revision": "f7269d63e7dc7acc8acba417687ac5e8",
+    "url": "/index.html"
+  }
+];
