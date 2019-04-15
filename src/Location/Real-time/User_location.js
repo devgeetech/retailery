4472192d2f4665b47ac086902edec45639dd403d
Dark0d3 <afreed178@gmail.com>
2019-04-15 14:19:58 +0530

    Location_added

---
+++ src/Location/Real-time/User_location.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import { error } from "util";
+
+type Props = {};
+class Location extends Component<Props> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      latitude: 0,
+      longitude: 0
+    };
+  }
+
+  componentDidMount() {
+    this.watchId = navigator.geolocation.watchPosition(
+      position => {
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude
+        });
+      },
+      error => {
+        this.setState({ error: error.message });
+      },
+      { enableHighAccuracy: true, timeout: 1, maximumAge: 1, distanceFilter: 1 }
+    );
+  }
+  render() {
+    return (
+      <div className="Location">
+        <header className="Location">
+          {this.state.latitude && this.state.longitude ? (
+            <text>
+              Mylocation is:{this.state.latitude},{this.state.longitude}
+            </text>
+          ) : (
+            <text>i dont know where u are</text>
+          )}
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Location;
