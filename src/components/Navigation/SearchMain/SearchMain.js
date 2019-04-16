8386af53062aa657c4bd5d157d99a77a8b7e28a9
devgeetech <devgeetech@gmail.com>
2019-04-17 03:48:36 +0530

    search, routing

--- src/components/Navigation/SearchMain/SearchMain.js
+++ src/components/Navigation/SearchMain/SearchMain.js
@@ -3,13 +3,12 @@ import React, { useState, useEffect } from 'react'
 // import Auxil from '../../hoc/Auxil/Auxil'
 // import FeedProd from './feedProd/FeedProd'
 // import classes from './FeedProdList.module.css'
-// import firebase from 'firebase'
+import firebase from 'firebase'
 // import Spinner from '../UI/Spinner/Spinner'
 import Input from '../../UI/Input/Input'
+import { withRouter } from 'react-router'
 
-
-const SearchMain = () => {
-
+const SearchMain = (props) => {
     const inputEl = {
             elementType: 'input',
             elementConfig: {
@@ -22,6 +21,30 @@ const SearchMain = () => {
 
     const [inpEl, chInp] = useState(inputEl)   
 
+    const srchHandler = (event) => {
+        event.preventDefault();
+
+        const queryParams = encodeURIComponent(inpEl.value)
+
+        props.history.push({
+        pathname : '/FeedM',
+        search : '?' + queryParams
+        })
+
+        // const rootRef = firebase.database().ref()
+        // const srchRef = rootRef.child('Products').orderByChild('name').equalTo(inpEl.value)
+        
+        // srchRef.once('value', function(snapshot) {
+        //     snapshot.forEach(function(childSnapshot) {
+        //       var childKey = childSnapshot.key;
+        //       var childData = childSnapshot.val();
+        //       console.log(childKey)
+        //       console.log(childData)
+        //     });
+        // });
+    }
+    
+    
     const inputChangedHandler = (event) => {
         const updatedFormElement = {...inpEl}
         updatedFormElement.value = event.target.value
@@ -31,7 +54,8 @@ const SearchMain = () => {
     }
 
     return(
-        <Input 
+        <form onSubmit={srchHandler}>
+            <Input 
             key='search'
             elementType='input'
             elementConfig={inpEl.elementConfig}
@@ -40,7 +64,9 @@ const SearchMain = () => {
             touched={inpEl.touched}
             shouldValidate={false}
             changed={(event) => inputChangedHandler(event)}/>
+        </form>
+        
     )
 }
 
-export default SearchMain
+export default withRouter(SearchMain)
