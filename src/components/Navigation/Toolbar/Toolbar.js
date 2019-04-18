16cc64063d40b544a426dcbf953cef94ed69b404
teamkinex <teamkinex2019@gmail.com>
2019-04-18 11:10:44 +0530

    toolbar

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
-import classes from './Toolbar.module.css'
+import classes from './Toolbar.module.scss'
 //import Logo from '../../Logo/Logo'
 import NavigationItems from '../NavigationItems/NavigationItems'
 import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle'
 import SearchMain from '../SearchMain/SearchMain'
+import icon from './icon.png'
 
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
@@ -13,6 +14,7 @@ const toolbar = (props) => (
             <Logo />
         </div> */}
         <SearchMain/>
+        <img src={icon} className={classes.Icon} />
         <nav className={classes.DesktopOnly}>
             <NavigationItems/>
         </nav>
