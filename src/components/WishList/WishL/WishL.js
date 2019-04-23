6d3e34f4c8b4e32ce58d167f8d52fbdfaa616f71
teamkinex <teamkinex2019@gmail.com>
2019-04-23 12:06:38 +0530

    sd

--- src/components/WishList/WishL/WishL.js
+++ src/components/WishList/WishL/WishL.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect, Component } from 'react'
 
 import Auxil from '../../../hoc/Auxil/Auxil'
 import classes from './WishL.module.scss'
-import FeedProd from '../../feedProdList/feedProd/FeedProd'
+import FeedProd from '../../feedProdList/feedProd/FeedProd.js'
 //import firebase from './node_modules/firebase'
 import Spinner from '../../UI/Spinner/Spinner'
 import firebase from 'firebase'
@@ -33,14 +33,17 @@ const WishL = (props) => {
                         .then(snapshot => {
                             indProdu.push(snapshot.data())
                             updComp(indProdu.map(indProd => (
-                                <div key={indProd.id}>
-                                    <FeedProd 
-                                        name={indProd.name}
-                                        content={indProd.content}
-                                        price={indProd.price}
-                                        imageSrc={indProd.imageSrc}
-                                        imgAlt={indProd.imgAlt}/>
-                                </div>
+                                <div key={indProd.id} className={classes.WishList}>
+                               
+                                <div className={classes.WfeedProd}>
+                                 <img src={props.imageSrc} alt={props.imgAlt} className={classes.Wimg}/>
+                                <div className={classes.contents}>
+                                 <h2><strong>{props.name}</strong></h2>
+                                 <p className={classes.cont}><i>{props.content}</i></p>
+                                 <p><strong>{props.price}</strong></p>
+                                 </div>
+                                 </div>
+                                 </div>
                             )))
                         })        
                 })
