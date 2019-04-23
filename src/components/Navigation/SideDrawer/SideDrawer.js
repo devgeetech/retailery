9d5090edc1f1790b8a34e1aa4d7876faf5b581c4
devgeetech <devgeetech@gmail.com>
2019-04-23 22:23:00 +0530

    app name

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -23,6 +23,7 @@ const sideDrawer = ( props ) => {
                     <Logo />
                 </div> */}
                 <nav>
+                    <h1 className={classes.headText}>ShoppingSpree</h1>
                     <NavigationItems clicked={props.closed} isAuthenticated={props.isAuth}/>
                 </nav>
             </div>
--- src/components/WishList/WishL/WishL.js
+++ src/components/WishList/WishL/WishL.js
@@ -1,13 +1,8 @@
 import React, { useState, useEffect, Component } from 'react'
 
 import Auxil from '../../../hoc/Auxil/Auxil'
-<<<<<<< HEAD
 import classes from './WishL.module.css'
 import FeedProd from '../../feedProdList/feedProd/FeedProd'
-=======
-import classes from './WishL.module.scss'
-import FeedProd from '../../feedProdList/feedProd/FeedProd.js'
->>>>>>> 2c7554e6096f3a6bc2680600c71c9b9599a95fcf
 //import firebase from './node_modules/firebase'
 import Spinner from '../../UI/Spinner/Spinner'
 import firebase from 'firebase'
@@ -38,7 +33,6 @@ const WishL = (props) => {
                         .then(snapshot => {
                             indProdu.push(snapshot.data())
                             updComp(indProdu.map(indProd => (
-<<<<<<< HEAD
                                 <div key={indProd.id} className={classes.FeedProd}>
                                     <FeedProd 
                                         name={indProd.name}
@@ -48,19 +42,6 @@ const WishL = (props) => {
                                         imgAlt={indProd.imgAlt}
                                         styleClass={classes}/>
                                 </div>
-=======
-                                // <div key={indProd.id} className={classes.WishList}>
-                               
-                                <div className={classes.WfeedProd}>
-                                 <img src={props.imageSrc} alt={props.imgAlt} className={classes.Wimg}/>
-                                <div className={classes.contents}>
-                                 <h2><strong>{props.name}</strong></h2>
-                                 <p className={classes.cont}><i>{props.content}</i></p>
-                                 <p><strong>{props.price}</strong></p>
-                                 </div>
-                                 </div>
-                                
->>>>>>> 2c7554e6096f3a6bc2680600c71c9b9599a95fcf
                             )))
                         })        
                 })
