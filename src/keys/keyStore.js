26f192cb127e4a782b5fddcde68a0bdaed8a829b
teamkinex <47789478+teamkinex@users.noreply.github.com>
2019-06-22 20:29:07 +0530

    keyStoreDummy

---
+++ src/keys/keyStore.js
@@ -0,0 +1,13 @@
+// INSERT YOUR FIREBASE DETAILS HERE!!!
+
+
+export const config = {
+    apiKey: "APIKey",
+    authDomain: "AuthDomain",
+    databaseURL: "databaseURL",
+    storageBucket: "storageBucket",
+    projectId: "projectId",
+    messagingSenderId: "Messaging"
+};
+
+export const mapKey = 'InsertYourGCPMapAPIKeyHere'
